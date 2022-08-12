89574e08b4c232e07c842f01ef67d3a0effa900f
Romulo Santos <romulosantos88@hotmail.com>
2022-08-12 16:19:38 +0100

    implement getOneBy

--- repositories/users.js
+++ repositories/users.js
@@ -60,13 +60,33 @@ class UsersRepository {
     Object.assign(record, attrs);
     await this.writeAll(records);
   }
+
+  async getOneBy(filters) {
+    const records = await this.getAll();
+
+    for (let record of records) {
+      let found = true;
+
+      for (let key in filters) {
+        if (record[key] !== filters[key]) {
+          found = false;
+        }
+      }
+
+      if (found) {
+        return record;
+      }
+    }
+  }
 };
 
 
 const test = async () => {
   const repo = new UsersRepository('users.json');
 
-  await repo.update('11111', { password: 'mypassword' })
+  const user = await repo.getOneBy({ password: 'mypassword' });
+
+  console.log(user);
 }
 
 test();
