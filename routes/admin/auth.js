840355af6f53035c600751d490e32bf1797e96c7
Romulo Santos <romulosantos88@hotmail.com>
2022-08-17 14:54:05 +0100

    sigin view

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const usersRepo = require('../../repositories/users');
 const signupTemplate = require('../../views/admin/auth/signup');
+const siginTemplate = require('../../views/admin/auth/sigin');
 
 const router = express.Router();
 
@@ -35,15 +36,7 @@ router.get('/signout', (req, res) => {
 });
 
 router.get('/signin', (req, res) => {
-  res.send(`
-    <div>
-      <form method="POST">
-        <input name="email" placeholder="email" />
-        <input name="password" placeholder="password" />
-        <button>Sign In</button>
-      </form>
-    </div>
-  `);
+  res.send(siginTemplate());
 });
 
 router.post('/signin', async (req, res) => {
